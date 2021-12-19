8e3652e5165fa11db524871b8af44d84dc1ae421
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2021-12-19 14:53:36 +0300

    creating a load screen

---
+++ app/src/main/java/ru/startandroid/develop/trysecond/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.startandroid.develop.trysecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getSupportActionBar().hide();
+    }
+}
