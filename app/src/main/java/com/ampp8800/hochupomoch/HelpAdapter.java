f15a967bd1596cf91574d8aacc2ae0d595030690
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2022-03-26 01:52:37 +0300

    design edit and add toast

--- app/src/main/java/com/ampp8800/hochupomoch/HelpAdapter.java
+++ app/src/main/java/com/ampp8800/hochupomoch/HelpAdapter.java
@@ -6,46 +6,61 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class HelpAdapter extends RecyclerView.Adapter<HelpAdapter.ViewHolder> {
+public class HelpAdapter extends RecyclerView.Adapter<HelpAdapter.HelpAdapterViewHolder> {
 
     private final LayoutInflater inflater;
-    private final List<Help> helps;
+    private final List<HelpListItem> helpListItem;
+    private final Context context;
 
-    HelpAdapter(Context context, List<Help> helps) {
-        this.helps = helps;
+    HelpAdapter(Context context, List<HelpListItem> helpListItem) {
+        this.helpListItem = helpListItem;
         this.inflater = LayoutInflater.from(context);
-    }
-    @Override
-    public HelpAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = inflater.inflate(R.layout.help_items, parent, false);
-        return new ViewHolder(view);
+        this.context = context;
     }
 
     @Override
-    public void onBindViewHolder(HelpAdapter.ViewHolder holder, int position) {
-        Help help = helps.get(position);
+    public HelpAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.help_item, parent, false);
+        return new HelpAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(HelpAdapterViewHolder holder, int position) {
+        HelpListItem help = helpListItem.get(position);
         holder.imageHelpView.setImageResource(help.getImageHelpResource());
         holder.nameView.setText(help.getName());
     }
 
     @Override
     public int getItemCount() {
-        return helps.size();
+        return helpListItem.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class HelpAdapterViewHolder extends RecyclerView.ViewHolder {
         final ImageView imageHelpView;
         final TextView nameView;
-        ViewHolder(View view){
+
+        HelpAdapterViewHolder(View view) {
             super(view);
             imageHelpView = view.findViewById(R.id.imageHelp);
             nameView = view.findViewById(R.id.name);
+            view.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    int positionIndex = getAdapterPosition();
+                    String name = helpListItem.get(positionIndex).getName();
+                    Toast toast = Toast.makeText(context, name, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/ampp8800/hochupomoch/MainActivity.java
+++ app/src/main/java/com/ampp8800/hochupomoch/MainActivity.java
@@ -1,23 +1,25 @@
 package com.ampp8800.hochupomoch;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Help> helps = new ArrayList<Help>();
+    private ArrayList<HelpListItem> helps = new ArrayList<HelpListItem>();
+
     @Override
     protected void onCreate(Bundle savedInstanceHelp) {
         super.onCreate(savedInstanceHelp);
         setContentView(R.layout.activity_help);
         // инициализация тулбара
-        Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // начальная инициализация списка
@@ -29,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
         // устанавливаем для списка адаптер
         recyclerView.setAdapter(adapter);
     }
+
     private void setInitialData() {
-        helps.add(new Help("Дети", R.drawable.children));
-        helps.add(new Help("Взрослые", R.drawable.adults));
-        helps.add(new Help("Пожилые", R.drawable.elderly));
-        helps.add(new Help("Животные", R.drawable.animals));
-        helps.add(new Help("Мероприятия", R.drawable.events));
+        helps.add(new HelpListItem("Дети", R.drawable.children));
+        helps.add(new HelpListItem("Взрослые", R.drawable.adults));
+        helps.add(new HelpListItem("Пожилые", R.drawable.elderly));
+        helps.add(new HelpListItem("Животные", R.drawable.animals));
+        helps.add(new HelpListItem("Мероприятия", R.drawable.events));
 
     }
 }
