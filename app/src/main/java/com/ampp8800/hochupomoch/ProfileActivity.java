1448773c280b06cfdd937c5f0997ec8f133d99fb
79997 <ampp441069>
2022-03-28 22:37:44 +0300

    working version, requires emulator verification

--- app/src/main/java/com/ampp8800/hochupomoch/ProfileActivity.java
+++ app/src/main/java/com/ampp8800/hochupomoch/ProfileActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,12 +25,22 @@ public class ProfileActivity extends AppCompatActivity {
         Context context = this;
         setContentView(R.layout.activity_profile);
 
-        // вставка изображения
+        // инициализация тулбара
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView textView = (TextView) toolbar.findViewById(R.id.toolbar_name);
+        textView.setText("Профиль");
+        setSupportActionBar(toolbar);
+        // добавить кнопку редактирования
+
+        // вставка изображения из сети
         ImageView targetImageView = (ImageView) findViewById(R.id.profile_image);
         Glide
                 .with(context)
                 .load("https://sun9-63.userapi.com/impf/c625318/v625318902/28050/-l1-yQ4qIQk.jpg?size=720x1080&quality=96&sign=d1a87aef8827f52ed841888c14b40e17&type=album")
                 .into(targetImageView);
+
+        // вставка текста из сети
+        TextView textView1 = (TextView) findViewById(R.id.profile_text_name);
 
         // начальная инициализация списка
         setInitialData();
