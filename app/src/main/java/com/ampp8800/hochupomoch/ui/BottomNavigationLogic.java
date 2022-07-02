6c0a059f90b30dfa6a03155336e557350f8fca92
ampp8800 <ampp8800@gmail.com>
2022-07-02 04:35:20 +0300

    work version(1)

--- app/src/main/java/com/ampp8800/hochupomoch/ui/BottomNavigationLogic.java
+++ app/src/main/java/com/ampp8800/hochupomoch/ui/BottomNavigationLogic.java
@@ -1,59 +1,115 @@
 package com.ampp8800.hochupomoch.ui;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.ampp8800.hochupomoch.R;
 
 
 public class BottomNavigationLogic {
-    private static Fragment helpFragment = HelpFragment.newInstance();
-    private static Fragment searchFragment = SearchFragment.newInstance();
-    private static Fragment profileFragment = ProfileFragment.newInstance();
-    private final static String MAIN_FRAGMENT = "mainFragment";
+    private Fragment helpFragment = HelpFragment.newInstance();
+    private Fragment searchFragment = SearchFragment.newInstance();
+    private Fragment profileFragment = ProfileFragment.newInstance();
+    private View newsButton;
+    private View searchButton;
+    private View helpButton;
+    private View historyButton;
+    private View profileButton;
+    private FragmentActivity fragmentActivity;
+    private View view;
+    private final String MAIN_FRAGMENT = "mainFragment";
+    private View buttonWasPreviouslyPress;
 
-    public static String getMainFragmentTag() {
-        return MAIN_FRAGMENT;
-    }
-
-    public static void initializeBottomBar(FragmentActivity fragmentActivity, @NonNull View view) {
-        View newsButton = (View) view.findViewById(R.id.news_button);
-        View searchButton = (View) view.findViewById(R.id.search_button);
-        View helpButton = (View) view.findViewById(R.id.help_button);
-        View historyButton = (View) view.findViewById(R.id.history_button);
-        View profileButton = (View) view.findViewById(R.id.profile_button);
+    public BottomNavigationLogic(FragmentActivity fragmentActivity, @NonNull View view) {
+        this.fragmentActivity = fragmentActivity;
+        this.view = view;
+        newsButton = (View) view.findViewById(R.id.news_button);
+        searchButton = (View) view.findViewById(R.id.search_button);
+        helpButton = (View) view.findViewById(R.id.help_button);
+        historyButton = (View) view.findViewById(R.id.history_button);
+        profileButton = (View) view.findViewById(R.id.profile_button);
 
         if (!searchFragment.isInLayout()) {
-            searchButton.setOnClickListener(clickedView -> fragmentActivity.
-                    getSupportFragmentManager().
-                    beginTransaction().
-                    replace(R.id.fragmentContainerView, searchFragment).commit());
+            searchButton.setOnClickListener(clickedView -> startSearchFragment());
         }
 
         if (!helpFragment.isInLayout()) {
-            helpButton.setOnClickListener(clickedView -> startMainFragment(fragmentActivity));
+            helpButton.setOnClickListener(clickedView -> startHelpFragment());
         }
 
         if (!profileFragment.isInLayout()) {
-            profileButton.setOnClickListener(clickedView -> fragmentActivity.
-                    getSupportFragmentManager().
-                    beginTransaction().
-                    replace(R.id.fragmentContainerView, profileFragment).commit());
+            profileButton.setOnClickListener(clickedView -> startProfileFragment());
         }
 
     }
 
-    public static void startMainFragment(@NonNull FragmentActivity fragmentActivity) {
+    public String getMainFragmentTag() {
+        return MAIN_FRAGMENT;
+    }
+
+    public int getIdOfPressedButton() {
+        if (buttonWasPreviouslyPress == null) {
+            return R.id.help_button;
+        }
+        return buttonWasPreviouslyPress.getId();
+    }
+
+    public void setButtonWasPreviouslyPress(View buttonWasPreviouslyPress) {
+        this.buttonWasPreviouslyPress = buttonWasPreviouslyPress;
+    }
+
+    public void startSearchFragment() {
+        fragmentActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, searchFragment).commit();
+        ((ImageView) view.findViewById(R.id.iv_search_button)).setImageResource(R.drawable.ic_search_green);
+        ((TextView) view.findViewById(R.id.tv_search_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.leaf));
+        removeColorSelection();
+        buttonWasPreviouslyPress = searchButton;
+    }
+
+    public void startHelpFragment() {
         fragmentActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, helpFragment, MAIN_FRAGMENT).commit();
+        ((ImageView) view.findViewById(R.id.iv_help_button)).setImageResource(R.drawable.green_circle);
+        ((TextView) view.findViewById(R.id.tv_help_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.leaf));
+        removeColorSelection();
+        buttonWasPreviouslyPress = helpButton;
+    }
+
+    public void startProfileFragment() {
+        fragmentActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, profileFragment).commit();
+        ((ImageView) view.findViewById(R.id.iv_profile_button)).setImageResource(R.drawable.ic_profile_green);
+        ((TextView) view.findViewById(R.id.tv_profile_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.leaf));
+        removeColorSelection();
+        buttonWasPreviouslyPress = profileButton;
+    }
+
+    private void removeColorSelection(){
+        if (searchButton.equals(buttonWasPreviouslyPress)) {
+            ((ImageView) view.findViewById(R.id.iv_search_button)).setImageResource(R.drawable.ic_search_grey);
+            ((TextView) view.findViewById(R.id.tv_search_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.warm_grey));
+        } else if (helpButton.equals(buttonWasPreviouslyPress)) {
+            ((ImageView) view.findViewById(R.id.iv_help_button)).setImageResource(R.drawable.red_circle);
+            ((TextView) view.findViewById(R.id.tv_help_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.warm_grey));
+        } else if (profileButton.equals(buttonWasPreviouslyPress)) {
+            ((ImageView) view.findViewById(R.id.iv_profile_button)).setImageResource(R.drawable.ic_profile_grey);
+            ((TextView) view.findViewById(R.id.tv_profile_button)).setTextColor(fragmentActivity.getResources().getColor(R.color.warm_grey));
+        }
+    }
+
+    public void showPreviousApp() {
+        if (searchButton.equals(buttonWasPreviouslyPress)) {
+            startSearchFragment();
+        } else if (helpButton.equals(buttonWasPreviouslyPress)) {
+            startHelpFragment();
+        } else if (profileButton.equals(buttonWasPreviouslyPress)) {
+            startProfileFragment();
+        }
     }
 
 }
--- app/src/main/java/com/ampp8800/hochupomoch/ui/MainActivity.java
+++ app/src/main/java/com/ampp8800/hochupomoch/ui/MainActivity.java
@@ -1,8 +1,6 @@
 package com.ampp8800.hochupomoch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,26 +11,38 @@ import com.ampp8800.hochupomoch.R;
 
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationLogic bottomNavigationLogic;
+    private final String ID_PRESS_BUTTON = "idPressButton";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            BottomNavigationLogic.startMainFragment(this);
-        }
         //логика работы нижней панели навигации
-        BottomNavigationLogic.initializeBottomBar(this, (View) findViewById(R.id.bottom_navigation));
+        bottomNavigationLogic = new BottomNavigationLogic(this, (View) findViewById(R.id.bottom_navigation));
+        //
+        if (savedInstanceState == null) {
+            bottomNavigationLogic.startHelpFragment();
+        } else {
+            bottomNavigationLogic.setButtonWasPreviouslyPress(findViewById(savedInstanceState.getInt(ID_PRESS_BUTTON)));
+            bottomNavigationLogic.showPreviousApp();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(ID_PRESS_BUTTON, bottomNavigationLogic.getIdOfPressedButton());
     }
 
     public void onBackPressed() {
-        if (getSupportFragmentManager().findFragmentByTag(BottomNavigationLogic.getMainFragmentTag()) != null) {
+        if (getSupportFragmentManager().findFragmentByTag(bottomNavigationLogic.getMainFragmentTag()) != null) {
             Intent startMain = new Intent(Intent.ACTION_MAIN);
             startMain.addCategory(Intent.CATEGORY_HOME);
             startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(startMain);
         } else {
-            BottomNavigationLogic.startMainFragment(this);
+            bottomNavigationLogic.startHelpFragment();
         }
     }
 
