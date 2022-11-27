fa81524142563cfdc9899e7cff1f08f5c60fcaf1
pereskokov <famous_lynx@mail.ru>
2022-11-27 12:38:49 +0300

    add rxJava test tasks

--- app/src/main/java/com/ampp8800/hochupomoch/db/AppDatabase.java
+++ app/src/main/java/com/ampp8800/hochupomoch/db/AppDatabase.java
@@ -3,7 +3,7 @@ package com.ampp8800.hochupomoch.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {NewsEntity.class}, version = 1)
+@Database(entities = {NewsEntity.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract NewsEntityDao newsEntityDao();
 }
---
+++ app/src/test/java/com/ampp8800/hochupomoch/FirstRxJavaTest.java
@@ -0,0 +1,61 @@
+package com.ampp8800.hochupomoch;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Combine numbersSource and lettersSource in one rx chain to receive proper result in each test.
+ * Write your chain by replacing resultObservable value with your solution
+ */
+public class FirstRxJavaTest {
+
+    private final Observable<Integer> numbersSource = Observable.just(1, 2, 3, 4, 5, 6, 7);
+    private final Observable<String> lettersSource = Observable.just("a", "b", "c", "d", "e", "f", "g");
+
+    @Test
+    public void combining1() {
+        // TODO Replace with your solution
+        Observable<String> resultObservable = Observable.error(new RuntimeException("Implement your solution"));
+
+        List<String> resultList = new ArrayList<>();
+        Disposable ignored = resultObservable.subscribe(resultList::add);
+        
+        // Expected result
+        Assert.assertEquals(resultList.size(), 7);
+        Assertions.assertIterableEquals(Arrays.asList("1a", "2b", "3c", "4d", "5e", "6f", "7g"), resultList);
+    }
+
+    @Test
+    public void combining2() {
+        // TODO Replace with your solution
+        Observable<String> resultObservable = Observable.error(new RuntimeException("Implement your solution"));
+
+        List<String> resultList = new ArrayList<>();
+        Disposable ignored = resultObservable.subscribe(resultList::add);
+
+        // Expected result
+        Assert.assertEquals(resultList.size(), 4);
+        Assertions.assertIterableEquals(Arrays.asList("1a", "3b", "5c", "7d"), resultList);
+    }
+
+    @Test
+    public void combining3() {
+        // TODO Replace with your solution
+        Observable<String> resultObservable = Observable.error(new RuntimeException("Implement your solution"));
+
+        List<String> resultList = new ArrayList<>();
+        Disposable ignored = resultObservable.subscribe(resultList::add);
+
+        // Expected result
+        Assert.assertEquals(resultList.size(), 4);
+        Assertions.assertIterableEquals(Arrays.asList("1A", "3C", "5E", "7G"), resultList);
+    }
+}
